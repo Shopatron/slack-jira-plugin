7d69de15d65f32bd745d580142ffc59cefecd9b2
James Elliott <jvelliott@users.noreply.github.com>
2016-07-27 10:16:49 -0700

    Finderbot

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config = {
   showIssueDetails: true,
   issueDetailsToShow: {'fields.summary':1 , 'fields.assignee' : 1, 'fields.creator' : 0, 'fields.description': 0},
   showDetailsByDefault: true,//if true, you don't need the '+' to get details
-  bot_name: "jira",//Provide the name to post under
+  bot_name: "Finderbot",//Provide the name to post under
   token: process.env.SLACK_TOKEN, // https://api.slack.com/web
   jira_urls: {
     // These should match projects from the projects property where you want to use a configuration other than the default
@@ -32,9 +32,9 @@ var config = {
   post: true,
   verbose: true,
   custom_texts: {
-    messagePrefix: "Hey, thought this might help: " //message you might like to prefix to JiraBot's post
+    messagePrefix: "Jira Issue: " //message you might like to prefix to JiraBot's post
   },
-  emoji: ":jira:", // be sure to upload your custom emoji in slack
+  emoji: ":finderbot:", // be sure to upload your custom emoji in slack
   link_separator: ", ",// use \n if you want new lines
   error_channel: '' //the id of the channel to send low level log errors.  If not defined, will use the current channel
 };
